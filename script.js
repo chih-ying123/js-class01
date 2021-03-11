a4ee48d98b845a51609c44d608e2a0946a9304be
chih-ying123 <g26350156@gmail.com>
2021-03-11 14:20:26 +0800

    OK

--- script.js
+++ script.js
@@ -37,15 +37,59 @@ let table_bg = document.querySelector('table');
 
 bgcolor_red.addEventListener('click',function(){
     body_bg.style.backgroundColor = 'rgba(255,0,0,.3)';
-    table_bg.style.backgroundColor = 'rgba(255,0,0,.5)';
+    table_bg.style.backgroundColor = 'rgba(255,0,0,.8)';
 });
 
 bgcolor_green.addEventListener('click',function(){
     body_bg.style.backgroundColor = 'rgba(0,180,0,.3)';
-    table_bg.style.backgroundColor = 'rgba(0,180,0,.5)';
+    table_bg.style.backgroundColor = 'rgba(0,180,0,.8)';
 });
 
 bgcolor_blue.addEventListener('click',function(){
     body_bg.style.backgroundColor = 'rgba(0,100,150,.3)';
-    table_bg.style.backgroundColor = 'rgba(0,100,150,.5)';
+    table_bg.style.backgroundColor = 'rgba(0,100,150,.8)';
+});
+
+/*--alert*2--*/
+document.querySelector('#alert_btn').addEventListener('click',function(){
+    let text_1 = document.querySelector('#text_1').value;
+    let text_2 = document.querySelector('#text_2').value;
+    alert (text_1);
+    alert (text_2);
+});
+
+/*--change_div color--*/
+var a=1;
+let div_group = document.querySelectorAll('.divcolor');
+document.querySelector('#divcolor_btn').addEventListener('click',function(){
+    if (a>0){
+        for (var i=0;i<div_group.length;i++){
+            div_group[i].style.backgroundColor = 'red';
+        }
+        a=a*-1
+    }else{
+        for (var i=0;i<div_group.length;i++){
+            div_group[i].style.backgroundColor = 'black';
+        }
+        a=a*-1
+    }
+});
+
+/*--hover_style--*/
+let hover_box = document.querySelector('.hover_box')
+hover_box.addEventListener('mouseenter',function(){
+    hover_box.classList.add('hover');
+});
+hover_box.addEventListener('mouseout',function(){
+    hover_box.classList.remove('hover');
+});
+
+/*--password--*/
+let checkbox1 = document.querySelector('.checkbox1')
+let note = document.querySelector('.note')
+checkbox1.addEventListener('mouseenter',function(){
+    note.style.display = 'block';
+});
+checkbox1.addEventListener('mouseout',function(){
+    note.style.display = 'none';
 });
